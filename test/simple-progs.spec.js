eedaa3b3775ef750ffd4f00af292b70eedc8f40a
Sergey Golovin <golovim@gmail.com>
2016-04-07 13:08:48 +0300

    now we can use recursion :)

--- test/simple-progs.spec.js
+++ test/simple-progs.spec.js
@@ -1,5 +1,25 @@
 const i = require('../../lib/interpreter.js');
 
 describe('simple progs', () => {
+  it('should be able to eval functions with recursion', () => {
+    const result = i.exec(`
+                          (define (test n)
+                            (if (< n 10)
+                              (do (set! n (+ n 1))
+                              (test n))))
+                          `);
+  });
+
+  it('should be should return a result from function with recursion', () => {
+    const result = i.exec(`
+                          (define (test n)
+                            (if (< n 10)
+                              (do (set! n (+ n 1))
+                                (test n))
+                               n))
+                          (test 0)
+                          `);
+    expect(result).toBe(10);
+  });
 });
 
