69b24e361a644ab674dbe884a4c21e33a2843920
Sergey Golovin <golovim@gmail.com>
2016-04-10 16:32:57 +0300

    add repl

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+const rl = require("readline");
+const prompts = rl.createInterface(process.stdin, process.stdout);
 const fileName = process.argv[2];
 const i = require('./lib/interpreter.js');
 
-fs.readFile(fileName, 'utf8', (err, data) => {
-  if (!err) {
-    console.log(i.exec(data));
-  } else {
-    console.log(err.message);
-  }
-});
+const repl = () => {
+  prompts.question('> ', code => {
+    try {
+      const result = i.exec(code);
+      console.log(result);
+    } catch(err){
+      console.error(err);
+    }
+    repl();
+  });
+}
+
+const execFile = () => {
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    if (!err) {
+      console.log(i.exec(data));
+    } else {
+      console.error(err.message);
+    }
+  });
+}
+
+if (fileName) {
+  execFile();
+} else {
+  repl();
+}
 
