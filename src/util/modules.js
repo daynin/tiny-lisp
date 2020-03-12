6c942b27d5c1240d81b88a00762dfc6b05f400fb
Sergey Golovin <golovim@gmail.com>
2020-03-12 18:54:07 +0300

    fix path resolution

--- src/util/modules.js
+++ src/util/modules.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
+const { resolve } = require('path');
+
+const resolvePath = path => resolve(__dirname, '../../', path);
 
 const loadModules = (path, ignoreFiles) =>
-  fs.readdirSync(path)
+  fs.readdirSync(resolvePath(path))
     .filter(path => ignoreFiles.every(ignoredPath => !path.includes(ignoredPath)))
-    .map(fileName => fs.readFileSync(`${path}/${fileName}`))
+    .map(fileName => fs.readFileSync(resolvePath(`${path}/${fileName}`)))
     .join(';');
 
 const loadModule = path => {
-  const content = fs.readFileSync(path).toString().trim();
+  const content = fs.readFileSync(resolvePath(path)).toString().trim();
 
   return content.endsWith(';')
     ? content.slice(0, content.length - 1)
